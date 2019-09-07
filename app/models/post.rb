f891a7a40a15be8b1d8abd092421019a225daff3
Solomon Feldman <solomonrfeldman@gmail.com>
2019-09-07 16:01:20 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validate :clickbait_validation
+  
+  CLICKBAIT_TERMS = [
+    /Won't Believe/i,
+    /Secret/i,
+    /Top [0-9]/i,
+    /Guess/i
+  ]
+  
+  def clickbait_validation
+    
+  end
 end
